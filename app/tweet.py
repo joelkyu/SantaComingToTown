af7c4c7bf324ce823e5be24a77453b94acfee8bc
Joe Yu <joe.yu@mappedin.ca>
2019-09-07 09:03:33 -0400

    created function to retrun tweets

--- app/tweet.py
+++ app/tweet.py
@@ -3,5 +3,8 @@ import tweepy
 auth = tweepy.OAuthHandler('rkEzbstvgQ2MpBYXdV5jgSqNx', 'WE2IC8CYvHlsqJ9PmMDa9WhiHe6YnxUW3zqQLarlaLVy5DZSUp')
 
 api = tweepy.API(auth)
-for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
-    print(tweet.text)
+
+def recentTweets(username):
+    return [tweet.text for tweet in tweepy.Cursor(api.search, q=username).items(20)]
+
+
