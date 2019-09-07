3394632bc32564f5612296c7709553b075146e9a
Joe Yu <joe.yu@mappedin.ca>
2019-09-07 14:01:04 -0400

    finished amazon item data gatherer

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 // this javascript file will be called as a content script from the manifest.json file. the content script will only be called on an amazon shop page.
 
+// IIFE that returns an array of objects with item name and price
+(function () {
+    var itemNames = document.getElementsByClassName("a-size-base-plus a-color-base a-text-normal");
+    var itemPricesWhole = document.getElementsByClassName("a-price-whole");
+    var itemPricesFraction = document.getElementsByClassName("a-price-fraction");   
 
-//Find Item Names
-var itemNames = document.getElementsByClassName("a-size-base-plus a-color-base a-text-normal");
+    var itemArray = []
 
-for (i=0; i < itemNames.length; i++) {
-    console.log(itemNames[i].textContent);
-};
+    for (i=0; i<itemNames.length; i++) {
+        itemArray.push({name: itemNames[i].textContent, price: parseFloat(itemPricesWhole[i].textContent + itemPricesFraction[i].textContent)});
+    };
 
-// Find Item Prices
-var itemPricesWhole = document.getElementsByClassName("a-price-whole");
-var itemPricesFraction = document.getElementsByClassName("a-price-fraction");
+    return itemArray
 
-for (i=0; i< itemPricesWhole.length; i++) {
-    console.log([itemPricesWhole[i].textContent + itemPricesFraction[i].textContent])
-};
+    // Test Function 
+    // var testItem = {name: itemNames[0].textContent,price: parseFloat(itemPricesWhole[0].textContent + itemPricesFraction[0].textContent)};
+    // console.log(testItem.name)
+    // console.log(testItem.price)
+
+})();
