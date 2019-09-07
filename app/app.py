3f2c2a513857a4e2338f9eb1cb70159054d8f605
alansun17904 <asun17904@gmail.com>
2019-09-07 14:04:48 -0400

    styling for index.html

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from config import app
 from models import db, Person, Characteristic
+import os
 
 
 @app.route('/')
 def index():
     db.create_all()  # Initiate all objects
     return render_template('index.html')
-
 
 
 @app.route('/add', methods=['POST'])
@@ -22,3 +22,17 @@ def add_person():
     db.session.commit()
     print(request.form)
     return render_template('index.html', people=Person.query.all())
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
