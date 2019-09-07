060eb8be852d79cb97547f7a68f9689060b5482f
Joe Yu <joe.yu@mappedin.ca>
2019-09-07 14:12:41 -0400

    fixed test cases

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // this javascript file will be called as a content script from the manifest.json file. the content script will only be called on an amazon shop page.
 
-// IIFE that returns an array of objects with item name and price
+// IIFE that returns an array of objects with Amazon item names and prices
 (function () {
     var itemNames = document.getElementsByClassName("a-size-base-plus a-color-base a-text-normal");
     var itemPricesWhole = document.getElementsByClassName("a-price-whole");
@@ -12,11 +12,8 @@
         itemArray.push({name: itemNames[i].textContent, price: parseFloat(itemPricesWhole[i].textContent + itemPricesFraction[i].textContent)});
     };
 
-    return itemArray
+    console.log(itemArray);
 
-    // Test Function 
-    // var testItem = {name: itemNames[0].textContent,price: parseFloat(itemPricesWhole[0].textContent + itemPricesFraction[0].textContent)};
-    // console.log(testItem.name)
-    // console.log(testItem.price)
+    return itemArray;
 
 })();
