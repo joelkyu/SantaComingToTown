493231d0347b68a28f794c9b6fddd73433c1c94c
Devin Ma <devin200011@gmail.com>
2019-09-08 01:54:59 -0400

    Finished fetch api

---
+++ js/api.js
@@ -0,0 +1,117 @@
+function indexPage () {
+    fetch(`http://52.54.115.82:8080/`, {method: "GET",})
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+        // Examine the text in the response
+        response.json().then(function(data) {
+            return data
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+}
+
+function add_person(twitter_handle, lower_price, upper_price) {
+    fetch(`http://52.54.115.82:8080/`, {
+        method: "POST",
+        headers: {
+            "accept": 'application/json',
+            twitter_handle, lower_price, upper_price
+        }
+    })
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+        // Examine the text in the response
+        response.json().then(function(data) {
+            console.log(data);
+            return data
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+};
+
+
+function compare(user_id, name, price) {
+    fetch(`http://52.54.115.82:8080/compare/${user_id}`, {
+        method: "GET",
+        headers: {
+            "accept": 'application/json',
+            'name': name,
+            'price': price
+        }
+    })
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+        // Examine the text in the response
+        response.json().then(function(data) {
+            console.log(data);
+            return data
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+};
+
+function get_person(user_id) {
+    fetch(`http://52.54.115.82:8080/person/${user_id}`, {method: "GET"})
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+        // Examine the text in the response
+        response.json().then(function(data) {
+            console.log(data);
+            return data
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+}
+
+function delete_person(user_id) {
+    fetch(`http://52.54.115.82:8080/delete/${user_id}`, {method: "POST"})
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+        // Examine the text in the response
+        response.json().then(function(data) {
+            console.log(data);
+            return data
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+    });
+}
